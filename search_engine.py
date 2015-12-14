a89ef53c306db64a6429cc40c196984b6269de91
Yishuang Liang <janeliang@airbears2-10-142-142-55.airbears2.1918.berkeley.edu>
2015-12-13 18:21:59 -0800

    added library hour function

--- search_engine.py
+++ search_engine.py
@@ -2,6 +2,51 @@ from lxml import html
 import requests
 
 CALDINING_URL='http://services.housing.berkeley.edu/FoodPro/dining/static/DiningMenus.asp?strCurLocation=0'
+LIBRARY_URL="http://www.lib.berkeley.edu/hours#"
+LIBRARIES=['anthropology library',
+ 'art history/classics library',
+ 'bancroft library/university archives',
+ 'berkeley law library',
+ 'bioscience & natural resources library',
+ 'business library',
+ 'career counseling library',
+ 'ced visual resources center',
+ 'chemistry library',
+ 'copy center',
+ 'data lab',
+ 'doe library',
+ 'earth sciences & map library',
+ 'east asian library',
+ 'education psychology library',
+ 'engineering library',
+ 'environmental design archives',
+ 'environmental design library',
+ 'ethnic studies library',
+ 'graduate services',
+ 'graduate theological union library',
+ 'institute for research on labor and employment library',
+ 'institute of governmental studies library',
+ 'institute of transportation studies library',
+ 'interlibrary services',
+ 'lawrence berkeley national laboratory library',
+ 'main stacks (gardner)',
+ 'mark twain papers & project',
+ 'mathematics statistics library',
+ 'media resources center',
+ 'moffitt library',
+ 'morrison library',
+ 'music library',
+ 'newspapers & microforms library',
+ 'northern regional library facility',
+ 'optometry and health sciences library',
+ 'pacific earthquake research (peer) center library',
+ 'pacific film archive library & film study center',
+ 'physics-astronomy library',
+ 'privileges desk',
+ 'public health library',
+ 'robbins collection',
+ 'social research library',
+ 'south/southeast asia library']
 
 def get_menu_items(dining_hall_id):
     url = CALDINING_URL + str(dining_hall_id)
@@ -16,5 +61,15 @@ def get_weather():
     info = page.json()
     temp = (info['main']['temp'] - 273.15) * 1.8 + 32
     return int(temp)
-def change(x):
-    return x + "gagaga"
+
+def get_library_hour(library):
+    page = requests.get(LIBRARY_URL)
+    html_tree = html.fromstring(page.content)
+    hours = html_tree.xpath('//td[@class="library-hours-block"]/text()')
+    new_hours = [x[3:len(x)-5] for x in hours if '-' in x or 'Closed' in x]
+    if len(LIBRARIES) != len(new_hours):
+        return "Not available"
+    else:
+        d = dict(zip(LIBRARIES, new_hours))
+    hour = [d[l] for l in d if library in l][0]
+    return hour
--- server.py
+++ server.py
@@ -1,6 +1,52 @@
 from flask import Flask, render_template, request
 import search_engine
+
 m = {"crossroads": 1, "cafe3": 3, "ckc": 4, "foothill": 6}
+libraries = ['anthropology library',
+ 'art history/classics library',
+ 'bancroft library/university archives',
+ 'berkeley law library',
+ 'bioscience & natural resources library',
+ 'business library',
+ 'career counseling library',
+ 'ced visual resources center',
+ 'chemistry library',
+ 'copy center',
+ 'data lab',
+ 'doe library',
+ 'earth sciences & map library',
+ 'east asian library',
+ 'education psychology library',
+ 'engineering library',
+ 'environmental design archives',
+ 'environmental design library',
+ 'ethnic studies library',
+ 'graduate services',
+ 'graduate theological union library',
+ 'institute for research on labor and employment library',
+ 'institute of governmental studies library',
+ 'institute of transportation studies library',
+ 'interlibrary services',
+ 'lawrence berkeley national laboratory library',
+ 'main stacks (gardner)',
+ 'mark twain papers & project',
+ 'mathematics statistics library',
+ 'media resources center',
+ 'moffitt library',
+ 'morrison library',
+ 'music library',
+ 'newspapers & microforms library',
+ 'northern regional library facility',
+ 'optometry and health sciences library',
+ 'pacific earthquake research (peer) center library',
+ 'pacific film archive library & film study center',
+ 'physics-astronomy library',
+ 'privileges desk',
+ 'public health library',
+ 'robbins collection',
+ 'social research library',
+ 'south/southeast asia library']
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,12 +55,20 @@ def home():
 
 @app.route('/', methods=['POST'])
 def search_post():
-    text = request.form['text']
+    text = request.form['text'].lower()
     string = search_engine.change(text)
     # search DH menus
     if text in m:
+        #to-do: can only search non-duplicates
         menu = search_engine.get_menu_items(m[text])
         return render_template('home_menu.html', items = menu)
+
+    # search library hours
+    for l in libraries:
+        if text in l.lower():
+            hour = search_engine.get_library_hour(text)
+            return render_template('home_lib.html', text=hour)
+
     # search local current weather
     if text == "weather":
         weather = search_engine.get_weather()
